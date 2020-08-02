4225b576459ba85c5b8c2206ed6fd97ce71e1dfc
Kile Deal <kile@kjdeal.dev>
2020-08-02 17:50:21 -0400

    bnyeh the sequel

--- main.py
+++ main.py
@@ -3,21 +3,35 @@ from typing import List
 
 from node import Node
 
+TOPOLOGY_SIZE = 5
 
-async def main():
-    print("Main!")
 
-    pool: List[Node] = list()
+class Topology:
+    def __init__(self, size):
+        self.pool: List[Node] = []
 
-    for _ in range(5):
-        pool.append(Node())
+        for _ in range(size):
+            self.pool.append(Node())
 
-    print("Pool initialized")
+    async def start_nodes(self):
+        await asyncio.gather(*(n.run() for n in self.pool))
 
-    await asyncio.gather(*[n.run() for n in pool])
 
-    print("Main ended")
+async def main_loop(topology: Topology):
+    print("Main Loop Started")
+    # TODO: listen for graceful exit
+    # asdfpP
+    while True:
+        await asyncio.sleep(5)
+        print("Main tick")
 
+
+async def main(size: int):
+    top = Topology(size)
+    await asyncio.gather(
+        main_loop(top),
+        top.start_nodes()
+    )
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(TOPOLOGY_SIZE))
--- node.py
+++ node.py
@@ -7,11 +7,17 @@ class Node(object):
 
     def __init__(self):
         self._id = next(Node.new_id)
+        self.num_q = asyncio.Queue()
 
     async def run(self):
         while True:
-            await asyncio.sleep(5)
-            print(f"{self} performed action")
+            await asyncio.sleep(2)
+            print(f'\t{self} is still running')
+            try:
+                item = self.num_q.get_nowait()
+                print(f"{self} got {item}")
+            except asyncio.QueueEmpty:
+                pass
 
     def __str__(self):
         return f'<Node {str(self._id)}>'
